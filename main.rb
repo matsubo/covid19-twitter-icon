853a2ef5bcf0b68cb328b14452bfd18058d97489
Yuki Matsukura <matsubokkuri@gmail.com>
2020-07-19 04:39:47 +0900

    Created

---
+++ main.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+Bundler.require
+
+def create(prefecture, filename)
+  puts filename
+  MiniMagick::Tool::Convert.new do |c|
+    c.size '550x550'
+    c << 'canvas:#FFFFFF'
+    c << '-fill' << '#000000'
+    c << '-gravity' << 'Center'
+
+    # covid-19
+    c << '-fill' << '#999999'
+    c << '-font' << 'fonts/Furore.otf'
+    c << '-pointsize' << '70'
+    c << '-annotate' << '+0-90' << 'COVID-19'
+
+    # prefecture
+    c << '-fill' << '#000000'
+    c << '-pointsize' << '120'
+    c << '-font' << 'fonts/rounded-mplus-2p-medium.ttf'
+    c << '-annotate' << '+0+20' << prefecture
+    c << "output/#{filename}.png"
+  end
+end
+
+require 'json'
+prefectures = {}
+File.open('prefectures.json') do |j|
+  prefectures = JSON.parse(j.read)
+end
+
+prefectures.each do |prefecture|
+  create(prefecture[0], prefecture[2])
+end
