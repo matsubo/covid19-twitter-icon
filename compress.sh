e7c65a8efaaaa866c51c25cd164d6124418ed049
Yuki Matsukura <matsubokkuri@gmail.com>
2020-07-23 01:02:18 +0900

    b equiet

--- compress.sh
+++ compress.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd output
-docker run  --rm -v $(pwd)/:/var/workdir/ kolyadin/pngquant --verbose --force --ext .png *.png
+docker run  --rm -v $(pwd)/:/var/workdir/ kolyadin/pngquant  --force --ext .png *.png
 
