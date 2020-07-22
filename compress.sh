367770b0259238fa9d7522575ac22913a549d399
Yuki Matsukura <matsubo@users.noreply.github.com>
2020-07-23 00:59:10 +0900

    Update compress.sh

--- compress.sh
+++ compress.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd output
-docker run -it --rm -v $(pwd)/:/var/workdir/ kolyadin/pngquant --verbose --force --ext .png *.png
+docker run  --rm -v $(pwd)/:/var/workdir/ kolyadin/pngquant --verbose --force --ext .png *.png
 
